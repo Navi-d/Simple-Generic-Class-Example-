30e424a8aaecc5e4d96d6d9702dba5543984a772
Navid <navidrazavi22@gmail.com>
2022-09-29 16:37:06 +0800

    Improved variable naming

--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ public class Main {
         Cat luna = new Cat("Luna", 2);
         Cat milo = new Cat("Milo", 2);
         Dog max = new Dog("Max", 2);
-        Action<Cat> action = new Action<>(luna);
-        Action<Dog> action2 = new Action<>(max);
+        Action<Cat> lunaCat = new Action<>(luna);
+        Action<Dog> maxDog = new Action<>(max);
 
-        action.printIdentification();
-        action.playWithSameType(milo);
-        action.eating("sth");
-        action2.eating("sth");
+        lunaCat.printIdentification();
+        lunaCat.playWithSameType(milo);
+        // can not use lunaCat.playWithSameType(max) because they are not the same type
+        lunaCat.eating("sth");
+        maxDog.eating("sth");
     }
 }
